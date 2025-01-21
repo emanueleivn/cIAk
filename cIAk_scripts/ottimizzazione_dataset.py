ed9fce68dfdf0b057f3dc7deec0b4b9c1dcb0af1
emanueleivn <e.iovane2@studenti.unisa.it>
2025-01-21 10:32:04 +0100

    Ottimizzazione del dataset Eliminazione colonne non significative, pulizia dei dati incompleti e aggiunta indicizzazione

---
+++ cIAk_scripts/ottimizzazione_dataset.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+file_path = '../../../Downloads/imdb-movies-dataset.csv'
+df = pd.read_csv(file_path)
+selected_columns = ['Title', 'Duration (min)', 'Genre', 'Rating', 'Director', 'Cast', 'Certificate']
+df_selected = df[selected_columns]
+
+df_selected.rename(columns={
+    'Title': 'titolo',
+    'Duration (min)': 'durata',
+    'Genre': 'genere',
+    'Rating': 'rating',
+    'Director': 'regista',
+    'Cast': 'cast',
+    'Certificate': 'classificazione'
+}, inplace=True)
+
+df_selected.drop_duplicates(inplace=True)
+df_selected.dropna(inplace=True)
+df_selected['movie_id'] = range(1, len(df_selected) + 1)
+output_file_path = 'dataset_ottimizzato.csv'
+df_selected.to_csv(output_file_path, index=False)
